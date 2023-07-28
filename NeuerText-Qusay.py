3eb2676616715cd9e02d32aab7b8a57652c34d2c
Qusay-Ku <133898916+Qusayku@users.noreply.github.com>
2023-07-28 14:39:20 +0200

    MessageToPublic ! #Qusay
    This is a message from me to the public !

---
+++ NeuerText-Qusay.py
@@ -0,0 +1,34 @@
+⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻ 
+  ⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻ 
+   ⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻
+    ⸻⸻⸻⸻⸻⸻⸻⸻⸻
+     ⸻⸻⸻⸻⸻⸻⸻⸻
+      ⸻⸻⸻⸻⸻⸻⸻
+       ⸻⸻⸻⸻⸻⸻
+        ⸻⸻⸻⸻⸻
+         ⸻⸻⸻⸻
+          ⸻⸻⸻
+           ⸻⸻
+            ⸻
+
+            I know that it is possible to fight this state! #Alhamdulillah ! 
+            This state is shit ! #Alhamdulillah !
+            They think they are big but they are weak ! #Alhamdulillah !
+            Hand up (shout ) ! -HuuuH ! ! ! -- HuHH ! ! ! ! ! ! ! ! ! ! !  ! ! ! ! ! !  ! ! ! !  ! ! ! !
+
+            #Qusay
+            #Ana Muwahhid
+
+            ⸻
+            ⸻⸻
+            ⸻⸻⸻
+            ⸻⸻⸻⸻
+            ⸻⸻⸻⸻⸻
+            ⸻⸻⸻⸻⸻⸻
+            ⸻⸻⸻⸻⸻⸻⸻⸻
+            ⸻⸻⸻⸻⸻⸻⸻⸻⸻
+            ⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻
+            ⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻ 
+            ⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻⸻ 
+                                              #Bitcoin : bc1qrlu0zaygf05t3lxtwmsq2q0hdwt5s5mrcc96hh #Qusay
+
